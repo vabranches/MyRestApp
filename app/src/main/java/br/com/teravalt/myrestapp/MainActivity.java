e31c01c63b4a90f02c476173553d3b7208f55ac4
vabranches <valter_abranches@yahoo.com.br>
2017-06-21 22:08:13 -0300

    + Layout + Main

--- app/src/main/java/br/com/teravalt/myrestapp/MainActivity.java
+++ app/src/main/java/br/com/teravalt/myrestapp/MainActivity.java
@@ -2,12 +2,37 @@ package br.com.teravalt.myrestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import br.com.teravalt.myrestapp.adapter.AndroidAdapter;
+import br.com.teravalt.myrestapp.api.AndroidAPI;
+import br.com.teravalt.myrestapp.model.Android;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvAndroids;
+    private AndroidAdapter androidAdapter;
+    private AndroidAPI androidAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvAndroids = (RecyclerView) findViewById(R.id.rvAndroids);
+
+        androidAdapter = new AndroidAdapter(new ArrayList<Android>());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rvAndroids.setLayoutManager(layoutManager);
+        rvAndroids.setAdapter(androidAdapter);
+        rvAndroids.setHasFixedSize(true);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        rvAndroids.addItemDecoration(itemDecoration);
+
+
     }
 }
--- app/src/main/java/br/com/teravalt/myrestapp/adapter/AndroidAdapter.java
+++ app/src/main/java/br/com/teravalt/myrestapp/adapter/AndroidAdapter.java
@@ -1,14 +1,65 @@
 package br.com.teravalt.myrestapp.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class AndroidAdapter {
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import br.com.teravalt.myrestapp.R;
+import br.com.teravalt.myrestapp.model.Android;
+
+public class AndroidAdapter extends RecyclerView.Adapter<AndroidAdapter.AndroidViewHolder>{
+
+    private List<Android> androids;
+
+    public AndroidAdapter(List<Android> a){
+        this.androids = a;
+    }
+
+    @Override
+    public AndroidViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View meuLayout = inflater.inflate(R.layout.android_row, parent,false);
+        return new AndroidViewHolder(meuLayout);
+    }
+
+    @Override
+    public void onBindViewHolder(AndroidViewHolder holder, int position) {
+        holder.tvTitulo.setText(androids.get(position).getNome());
+        holder.tvSubtitulo.setText("Versão: "+androids.get(position).getVersao()+" - API: "+androids.get(position).getApi());
+
+
+        Picasso.with(holder.itemView.getContext())
+                .load(androids.get(position).getUrlImagem())
+                .placeholder(R.drawable.carregando)
+                .error(R.drawable.ops2)
+                .into(holder.ivImagem);
+    }
+
+    @Override
+    public int getItemCount() {
+        return androids.size();
+    }
 
     public class AndroidViewHolder extends RecyclerView.ViewHolder{
 
+        public ImageView ivImagem;
+        public TextView tvTitulo;
+        public TextView tvSubtitulo;
+
         public AndroidViewHolder(View itemView) {
             super(itemView);
+
+            ivImagem = (ImageView) itemView.findViewById(R.id.ivImagem);
+            tvTitulo = (TextView) itemView.findViewById(R.id.tvTitulo);
+            tvSubtitulo = (TextView) itemView.findViewById(R.id.tvSubTitulo);
         }
     }
 
